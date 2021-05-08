d166fcb5a41c3d0dba363fc5e9b009591fcb1071
Simonarde Lima <simonarde@gmail.com>
2021-05-08 13:06:19 -0300

    adicionando model de pedido

--- api/models/pedido.js
+++ api/models/pedido.js
@@ -1,7 +1,31 @@
 const mongoose = require('mongoose');
 
-var pedidoSchema = new mongoose.Schema({
+var produtoSubSchema = new mongoose.Schema(
+  {
+    idProduto: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "produtos",
+    },
+    quantidade: {
+      type: Number,
+      default: 1,
+    },
+    comentario: String,
+  },
+  { _id: false }
+);
 
+
+var pedidoSchema = new mongoose.Schema({
+  data: {
+    type: Date,
+    default: Date.now,
+  },
+  nomeUsuario: {
+    type: String,
+    required: true,
+  },
+  lista: [produtoSubSchema],
 });
 
 module.exports = mongoose.model('Pedido', pedidoSchema);
