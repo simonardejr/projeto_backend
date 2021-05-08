1f9ea20e68e443aa40dbb099aee294153dcca7f2
Simonarde Lima <simonarde@gmail.com>
2021-05-08 17:33:49 -0300

    adicionando pedidos

--- api/routes/pedidos.js
+++ api/routes/pedidos.js
@@ -1,28 +1,105 @@
 const express = require("express");
-const router = express.Router();
+const routes = express.Router();
 
-router.get("/", function (req, res) {
-  const { name, description } = req.body;
-  res.send(`GET pedidos`);
+const Pedido = require("../models/pedido");
+
+const qtElementosNaPagina = 2;
+
+
+/**
+ * GET /pedidos
+ * Lista de pedidos
+ */
+routes.get("/", async (req, res) => {
+  const { pagina } = req.query;
+  const elementosPulados = (pagina - 1) * qtElementosNaPagina;
+
+  try {
+
+    const doc = await Pedido.find()
+      .populate("lista.idProduto")
+      .sort({
+        data: -1, // menor para o maior / -1, // maior para o menor
+      })
+      .skip(elementosPulados)
+      .limit(qtElementosNaPagina)
+      .select("-permiteAlteracao -__v")
+
+    res.send(doc);
+
+  } catch (err) {
+    res.status(500).send({ mensagem: err.message, erro: err });
+  }
+
 });
 
-router.post("/", function (req, res) {
-  const { name, description } = req.body;
-  res.send(`POST pedidos`);
+
+/**
+ * POST /pedidos
+ * Inserção de pedidos
+ */
+routes.post("/", async (req, res) => {
+  const { nomeUsuario, lista } = req.body;
+
+  try {
+
+    const pedido = new Pedido({
+      nomeUsuario: nomeUsuario,
+      lista: lista,
+    });
+
+    const doc = await pedido.save();
+
+    res.status(204).send({});
+  } catch (err) {
+    res.status(500).send({ mensagem: err.message, erro: err });
+  }
+
 });
 
-router.get("/:idPedido", (req, res) => {
+
+/**
+ * GET /pedidos/id
+ * Informações do pedido informado
+ */
+routes.get("/:idPedido", async function (req, res) {
   const { idPedido } = req.params;
-  res.send({
-    mensagem: idPedido,
-  });
+
+  try {
+
+    const doc = await Pedido.find({
+      _id: idPedido,
+    }).populate("lista.idProduto");
+
+    res.send(doc);
+
+  } catch (err) {
+    res.status(500).send({ mensagem: err.message, erro: err });
+  }
+
 });
 
-router.delete("/:idPedido", (req, res) => {
+
+/**
+ * DELETE /pedidos/id
+ * Deleta o pedido informado
+ */
+routes.delete("/:idPedido", async function (req, res) {
   const { idPedido } = req.params;
-  res.send({
-    mensagem: idPedido,
-  });
+
+  try {
+
+    const doc = await Pedido.deleteOne({
+      _id: idPedido,
+    });
+
+    res.status(204).send({});
+
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ mensagem: err.message });
+  }
+
 });
 
-module.exports = router;
+module.exports = routes;
