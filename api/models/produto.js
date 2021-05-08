df6de48bf688104bba157c01d0f3d3b48525ae60
Simonarde Lima <simonarde@gmail.com>
2021-05-08 13:08:23 -0300

    ajuste de tamanho da identação

--- api/models/produto.js
+++ api/models/produto.js
@@ -1,25 +1,25 @@
 const mongoose = require('mongoose');
 
 var produtoSchema = new mongoose.Schema({
-    nome: {
-        type: String,
-        required: true,
-        unique: true,
-        index: true,
-    },
-    preco: {
-        type: Number,
-        required: true
-    },
-    descricao: {
-        type: String,
-        required: true
-    },
-    permiteAlteracao: {
-        type: Boolean,
-        default: true,
-    },
-    imagem: String
+  nome: {
+    type: String,
+    required: true,
+    unique: true,
+    index: true,
+  },
+  preco: {
+    type: Number,
+    required: true
+  },
+  descricao: {
+    type: String,
+    required: true
+  },
+  permiteAlteracao: {
+    type: Boolean,
+    default: true,
+  },
+  imagem: String
 });
 
 module.exports = mongoose.model('Produto', produtoSchema);
